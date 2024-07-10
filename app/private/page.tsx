315229780272c625d995ee0056adfea45d742ca9
Andrew Smith <a.smith@silentworks.co.uk>
2024-07-10 22:25:18 +0000

    Testing private page

--- app/private/page.tsx
+++ app/private/page.tsx
@@ -1,14 +1,17 @@
-import { redirect } from 'next/navigation'
-
-import { createClient } from '@/utils/supabase/server'
+// import { redirect } from 'next/navigation'
+// import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
 
 export default async function PrivatePage() {
-  const supabase = createClient()
+  // const supabase = createClient()
 
-  const { data, error } = await supabase.auth.getUser()
-  if (error || !data?.user) {
-    redirect('/login')
-  }
+  // const { data, error } = await supabase.auth.getUser()
+  // if (error || !data?.user) {
+  //   redirect('/login')
+  // }
 
-  return <p>Hello {data.user.email}</p>
+  // return <p>Hello {data.user.email}</p>
+  const cookieData = cookies().getAll()
+  console.log({ cookieData })
+  return <h1>Hello World!!!</h1>
 }
